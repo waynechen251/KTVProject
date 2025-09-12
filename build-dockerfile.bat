06562ef80d6b38ebe0ead17f693ed7acc0eda470
waynechen251 <waynechen251@gmail.com>
2025-09-13 04:07:09 +0800

    調整目錄結構

--- build-dockerfile.bat
+++ build-dockerfile.bat
@@ -6,9 +6,11 @@ set "IMAGE_NAME=mini-ktv"
 set "IMAGE=%REGISTRY%/mini-ktv/%IMAGE_NAME%:latest"
 
 echo Building %IMAGE_NAME%...
+cd src
 docker build -f Dockerfile -t %IMAGE% .
 if errorlevel 1 (
   echo [ERROR] %IMAGE_NAME% image build failed.
+  cd ..
   exit /b 1
 )
 
@@ -16,8 +18,10 @@ echo Pushing %IMAGE_NAME% image...
 docker push %IMAGE%
 if errorlevel 1 (
   echo [ERROR] %IMAGE_NAME% image push failed.
+  cd ..
   exit /b 1
 )
 
 echo %IMAGE% built and pushed successfully.
+cd ..
 exit /b 0
